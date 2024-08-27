1d7b0473d4864bafd4cf890a331ade7d09b36668
Ambar Gonzalez <49761975+Ambarpg80@users.noreply.github.com>
2024-08-27 15:19:44 -0400

    solved fibonacci

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    my_list = []
+    i = 0
+    if length == 0:
+        print(my_list)   
+    elif length == 1:
+        my_list.append(0)
+        print(my_list)
+    elif length >= 2:
+        my_list.append(0)
+        my_list.append(1) 
+        while len(my_list) < length:
+            my_list.append(my_list[-1] + my_list[-2])
+            i+=1       
+        print(my_list)  
